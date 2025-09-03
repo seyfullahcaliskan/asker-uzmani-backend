bf40b95285f7299c6785f78af7961d58b6e539d4
seyfullahcaliskan <seyfullah.caliskan@tatmetal.com.tr>
2025-09-03 16:54:12 +0300

    First commit

---
+++ src/main/java/com/askeruzmani/asker_uzmani_backend/Entities/NavlinksEntity.java
@@ -0,0 +1,11 @@
+package com.askeruzmani.asker_uzmani_backend.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "navlinks")
+public class NavlinksEntity extends BaseEntity {
+
+
+}
